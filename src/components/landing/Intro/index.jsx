6e6e4b6684eb804cf40dd24810b8c1c2354539b8
Scott Schindler <scottschindler29@gmail.com>
2020-02-24 23:59:45 -0500

    changes

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -11,7 +11,7 @@ export const Intro = () => (
     <IntroWrapper as={Container}>
       <Details>
         <h1>Hi There!</h1>
-        <h4>I’m Scott and I love building digital products and having a great time!</h4>
+        <h4>I’m Scott and I love building digitfbmvfgkal products and having a great time!</h4>
         <Button as={AnchorLink} href="#contact">
           Hire me
         </Button>
