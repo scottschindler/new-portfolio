18cac568260e8d4608953b6fa4436429b957d804
Scott Schindler <scottschindler29@gmail.com>
2020-04-02 16:44:17 -0400

    updated so that github projects is displayed

--- src/components/landing/Projects/index.jsx
+++ src/components/landing/Projects/index.jsx
@@ -5,40 +5,41 @@ import starIcon from 'assets/icons/star.svg';
 import forkIcon from 'assets/icons/fork.svg';
 import { Wrapper, Grid, Item, Content, Stats } from './styles';
 
-export const Projects = () => (
-  // const {
-  //   github: {
-  //     viewer: {
-  //       repositories: { edges },
-  //     },
-  //   },
-  // } = useStaticQuery(
-  //   graphql`
-  //     {
-  //       github {
-  //         viewer {
-  //           repositories(first: 8, orderBy: { field: STARGAZERS, direction: DESC }) {
-  //             edges {
-  //               node {
-  //                 id
-  //                 name
-  //                 url
-  //                 description
-  //                 stargazers {
-  //                   totalCount
-  //                 }
-  //                 forkCount
-  //               }
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   `
-  // );
-  <Wrapper as={Container} id="projects">
-    <h2>Projects</h2>
-    {/* <Grid>
+export const Projects = () => {
+  const {
+    github: {
+      viewer: {
+        repositories: { edges },
+      },
+    },
+  } = useStaticQuery(
+    graphql`
+      {
+        github {
+          viewer {
+            repositories(first: 8, orderBy: { field: STARGAZERS, direction: DESC }) {
+              edges {
+                node {
+                  id
+                  name
+                  url
+                  description
+                  stargazers {
+                    totalCount
+                  }
+                  forkCount
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+  );
+  return (
+    <Wrapper as={Container} id="projects">
+      <h2>Projects</h2>
+      <Grid>
         {edges.map(({ node }) => (
           <Item key={node.id} as="a" href={node.url} target="_blank" rel="noopener noreferrer">
             <Card>
@@ -59,6 +60,7 @@ export const Projects = () => (
             </Card>
           </Item>
         ))}
-      </Grid> */}
-  </Wrapper>
-);
+      </Grid>
+    </Wrapper>
+  );
+};
